8ebada9f7f010a5388f1f35f1b90423f35f86d52
reju-1 <rahmed1445@gmail.com>
2024-11-03 21:32:34 +0600

    code refactoring

--- src/client_handler.py
+++ src/client_handler.py
@@ -4,22 +4,21 @@ from typing import Tuple
 from utility.request_parser import request_parser
 from utility.response import find_file, generate_headers, send_file_response
 
-
 # type alias ip:port
 socket_address = Tuple[str, int]
 
 
 def client_handler(client_socket: socket.socket, address: socket_address) -> None:
     print(f"Accepted connection from {address[0]}:{address[1]}")
-    
+
     headers, body = request_parser(client_socket)
 
-    # print(f"request headers: {headers}")
-    print(f"\nrequest url: {headers["url"]}")
+    # Print request details
+    print(f"\nrequest url: {headers['url']}")
     print(f"request body: {body}\n")
 
     # Todo: Save Request body
 
     res_file = find_file(headers["url"])
     res_headers = generate_headers(res_file)
-    send_file_response(client_socket,res_headers, res_file )
+    send_file_response(client_socket, res_headers, res_file)

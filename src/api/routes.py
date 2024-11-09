4f8e06ac64a2cc1c8891edcce66a9232e8b9aa30
reju-1 <rahmed1445@gmail.com>
2024-11-09 23:31:00 +0600

    introduce REST Api

---
+++ src/api/routes.py
@@ -0,0 +1,63 @@
+import json
+import socket
+from pathlib import Path
+from datetime import datetime
+
+content_path = Path("./public")
+
+
+def send_json_response(socket: socket.socket, headers: dict[str, any]) -> None:
+    """Send json response to client"""
+
+    file_info = _get_files(headers["url"])
+
+    file_info_json = json.dumps(file_info)
+
+    response_headers = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: application/json\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+    )
+
+    try:
+        socket.sendall(response_headers.encode("utf-8"))
+        socket.sendall(file_info_json.encode("utf-8"))
+
+    finally:
+        socket.close()
+
+
+def _get_files(path: str = "/") -> json:
+    """Get all the file in given path along with name, size, las_modified"""
+
+    files = []
+    for f in content_path.iterdir():
+        if f.is_file():
+            file_info = {
+                "file-name": f.name,
+                "size": _convert_size(f.stat().st_size),
+                "modified": datetime.fromtimestamp(f.stat().st_mtime).strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                ),
+            }
+            files.append(file_info)
+
+    return files
+
+
+def _convert_size(size_bytes: int) -> str:
+    """Convert number of Bytes to B/KB/MB/GB"""
+
+    if size_bytes == 0:
+        return "0 B"
+
+    # Defining units in increments of 1024
+    size_units = ["B", "KB", "MB", "GB", "TB"]
+    i = 0
+
+    while size_bytes >= 1024 and i < len(size_units) - 1:
+        size_bytes /= 1024
+        i += 1
+
+    return f"{size_bytes:.1f} {size_units[i]}"
--- src/client_handler.py
+++ src/client_handler.py
@@ -5,7 +5,7 @@ from globals import LOG_ADDRESS
 from middleware.request_parser import request_parser
 from utility.logger import log_request
 from utility.response import find_file, send_file_response
-
+from api.routes import send_json_response
 
 # type alias ip:port
 socket_address = Tuple[str, int]
@@ -23,6 +23,9 @@ def client_handler(client_socket: socket.socket, address: socket_address) -> Non
     print(f"request body: {body}\n")
 
     log_request(LOG_ADDRESS, headers, body)
+
+    if headers.get("Accept") == "application/json":
+        return send_json_response(socket=client_socket, headers=headers)
 
     res_file = find_file(headers["url"])
     send_file_response(client_socket, res_file)
